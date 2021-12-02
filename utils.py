277ee4b3254bce9074244ecf3c18dba674b1fa74
Thomas Schmitt <t.e.schmitt@gmail.com>
2021-12-02 12:54:37 +0100

    :recycle: Refactor duplicate code in stats and graph to utility function

--- utils.py
+++ utils.py
@@ -1,6 +1,8 @@
 from glob import glob
 from pathlib import PurePath
 
+import pandas as pd
+
 
 def get_file_list(glob_string, report_dir):
     """Get a list of files in directory :param report_dir:
@@ -14,3 +16,21 @@ def get_file_list(glob_string, report_dir):
     file_lists = [glob(g.as_posix()) for g in globs]
     file_list = [i for sublist in file_lists for i in sublist]  # flatten a two-dimensional list
     return file_list
+
+
+def get_dataframe_from_file_list(file_list):
+    file_handles = [open(fp, 'r') for fp in file_list]
+    msg_stats = []
+    for fh in file_handles:
+        scenario = {'scenario': fh.readline().split(' ')[-1].strip()}
+        for line in fh.readlines():
+            elems = line.split(':')
+            if len(elems) > 1:
+                scenario[elems[0].strip()] = elems[1].strip()
+        msg_stats.append(scenario)
+        fh.close()
+    stats_df = pd.DataFrame(msg_stats).set_index('scenario').sort_index()
+    for col in stats_df.columns:
+        if col != 'scenario':
+            stats_df[col] = pd.to_numeric(stats_df[col], errors='coerce')
+    return stats_df
