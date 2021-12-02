afa7e7864389f7b850e95cc0cca57b0856318b06
Thomas Schmitt <t.e.schmitt@gmail.com>
2021-12-02 18:05:45 +0100

    :recycle: Graphs now more configurable, defaults now fetched from config

--- commands/graph.py
+++ commands/graph.py
@@ -3,26 +3,37 @@ import click
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-from config import seaborn_themes, stat_options, seaborn_contexts
+from config import seaborn_palettes, seaborn_styles, stat_options, seaborn_contexts, DEFAULT_GLOB, \
+    DEFAULT_GRAPH_CONTEXT, DEFAULT_OUTPUT_DIR, DEFAULT_REPORTS_DIR, DEFAULT_GRAPH_OUTPUT_FMT, DEFAULT_GRAPH_STYLE, \
+    DEFAULT_STAT, DEFAULT_GRAPH_PALETTE
 from utils import get_file_list, get_dataframe_from_file_list
 
 
 @click.command('graph')
-@click.option('-c', '--context', default='paper', type=click.Choice(sorted(seaborn_contexts)),
-              help='Seaborn context for the generated graphs')
-@click.option('-d', '--report-dir', default='./reports/', type=click.Path(exists=True), help='Report directory.')
-@click.option('-f', '--output-format', default='PNG', type=click.Choice(['PNG', 'JPG'], case_sensitive=False))
-@click.option('-g', '--glob', 'glob_string', default=['*MessageStats*.txt'], multiple=True,
-              help='Glob pattern to look for in reports directory.')
-@click.option('-o', '--output-dir', default='./images/', type=click.Path(exists=True), help='Output directory.')
-@click.option('-s', '--stat', default='delivery_prob', type=click.Choice(sorted(stat_options.keys())),
-              help='Name of the statistics value that should be parsed from the report files')
-@click.option('-t', '--theme', default='whitegrid', type=click.Choice(sorted(seaborn_themes)),
-              help='Seaborn theme for the generated graphs')
-def graph(report_dir, glob_string, output_format, output_dir, stat, theme, context):
+@click.option('-d', '--report-dir', default=DEFAULT_REPORTS_DIR, type=click.Path(exists=True), help='Report directory.',
+              show_default=True)
+@click.option('-f', '--output-format', default=DEFAULT_GRAPH_OUTPUT_FMT,
+              type=click.Choice(['PNG', 'JPG'], case_sensitive=False),
+              show_default=True)
+@click.option('-g', '--glob', 'glob_string', default=DEFAULT_GLOB, multiple=True,
+              help='Glob pattern to look for in reports directory.', show_default=True)
+@click.option('-o', '--output-dir', default=DEFAULT_OUTPUT_DIR, type=click.Path(exists=True), help='Output directory.',
+              show_default=True)
+@click.option('-s', '--stat', default=DEFAULT_STAT, type=click.Choice(sorted(stat_options.keys())), multiple=True,
+              help='Name of the statistics value that should be parsed from the report files', show_default=True)
+@click.option('-c', '--context', default=DEFAULT_GRAPH_CONTEXT, type=click.Choice(sorted(seaborn_contexts)),
+              help='Seaborn context for the generated graphs', show_default=True)
+@click.option('-p', '--palette', default=DEFAULT_GRAPH_PALETTE, type=click.Choice(sorted(seaborn_palettes)),
+              help='Seaborn color palette for the generated graphs', show_default=True)
+@click.option('-y', '--style', default=DEFAULT_GRAPH_STYLE, type=click.Choice(sorted(seaborn_styles)),
+              help='Seaborn theme for the generated graphs', show_default=True)
+def graph(report_dir, glob_string, output_format, output_dir, stat, style, context, palette):
     """Draw graphs based on the generated report files
     \f
 
+    :param palette: Seaborn palette for the generated graphs
+    :param context: Seaborn context for the generated graphs
+    :param style: Seaborn style for the generated graphs
     :param stat: name of the statistics value that should be parsed from the report files
     :param output_dir: output directory
     :param output_format: output format
@@ -33,9 +44,17 @@ def graph(report_dir, glob_string, output_format, output_dir, stat, theme, conte
     file_list = get_file_list(glob_string, report_dir)
     stats_df = get_dataframe_from_file_list(file_list)
 
-    sns.set_theme(style="whitegrid", context="paper")
-    ax = sns.barplot(x=stats_df.index, y=stats_df[stat], palette="deep")
-    ax.axhline(0, color="k", clip_on=False)
-    ax.set_ylabel(stat_options[stat])
-    ax.set_xlabel("Scenario")
+    for i in range(len(stat)):
+        st = stat[i]
+        sns.set_theme(style=style, context=context)
+        fig = sns.barplot(x=stats_df.index, y=stats_df[st], palette=palette)
+        # ax.axhline(0, color="k", clip_on=False)
+        fig.set_title(stat_options[st])
+        fig.set_ylabel(stat_options[st])
+        fig.set_xlabel("Scenario")
+
+        # call this only between to plots, never at the end
+        if i < len(stat) - 1:
+            plt.figure()
+
     plt.show()
--- commands/stats.py
+++ commands/stats.py
@@ -1,16 +1,18 @@
 import click
 
-from config import stat_options
+from config import stat_options, DEFAULT_GLOB, DEFAULT_OUTPUT_DIR, DEFAULT_REPORTS_DIR, DEFAULT_STAT
 from utils import get_file_list, get_dataframe_from_file_list
 
 
 @click.command('stats')
-@click.option('-d', '--report-dir', default='./reports/', type=click.Path(exists=True), help='Report directory.')
-@click.option('-g', '--glob', 'glob_strings', default=['*MessageStats*.txt'], multiple=True,
-              help='Glob pattern(s) to look for in reports directory.')
-@click.option('-o', '--output-dir', default='./images/', type=click.Path(exists=True), help='Output directory.')
-@click.option('-s', '--stat', default=['delivery_prob'], type=click.Choice(sorted(stat_options.keys())), multiple=True,
-              help='Name of the statistics value(s) that should be parsed from the report files')
+@click.option('-d', '--report-dir', default=DEFAULT_REPORTS_DIR, type=click.Path(exists=True), help='Report directory.',
+              show_default=True)
+@click.option('-g', '--glob', 'glob_strings', default=DEFAULT_GLOB, multiple=True,
+              help='Glob pattern(s) to look for in reports directory.', show_default=True)
+@click.option('-o', '--output-dir', default=DEFAULT_OUTPUT_DIR, type=click.Path(exists=True), help='Output directory.',
+              show_default=True)
+@click.option('-s', '--stat', default=DEFAULT_STAT, type=click.Choice(sorted(stat_options.keys())), multiple=True,
+              help='Name of the statistics value(s) that should be parsed from the report files', show_default=True)
 @click.option('-t', '--separate-tables', default=False, is_flag=True, help="Show all stats in separate tables")
 def stats(report_dir, separate_tables, glob_strings, output_dir, stat):
     """Get stats from the generated report files
