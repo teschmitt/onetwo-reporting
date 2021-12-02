fcfb45ec659f7ce809ba6e88ac8327bffe2866ca
Thomas Schmitt <t.e.schmitt@gmail.com>
2021-12-02 12:51:55 +0100

    :sparkles: Makes stat arg work #8

--- commands/stats.py
+++ commands/stats.py
@@ -1,50 +1,36 @@
 import click
-import pandas as pd
 
 from config import stat_options
-from utils import get_file_list
+from utils import get_file_list, get_dataframe_from_file_list
 
 
 @click.command('stats')
 @click.option('-d', '--report-dir', default='./reports/', type=click.Path(exists=True), help='Report directory.')
-@click.option('-f', '--output-format', default='PNG', type=click.Choice(['PNG', 'JPG'], case_sensitive=False))
-@click.option('-g', '--glob', 'glob_string', default=['*'], multiple=True,
-              help='Glob pattern to look for in reports directory.')
+@click.option('-g', '--glob', 'glob_strings', default=['*'], multiple=True,
+              help='Glob pattern(s) to look for in reports directory.')
 @click.option('-o', '--output-dir', default='./images/', type=click.Path(exists=True), help='Output directory.')
-@click.option('-s', '--stat', default='sim_time', type=click.Choice(sorted(stat_options.keys())),
-              help='Name of the statistics value that should be parsed from the report files')
-def stats(report_dir, glob_string, output_format, output_dir, stat):
-    """
-    Get stats from the generated report files
+@click.option('-s', '--stat', default=['sim_time'], type=click.Choice(sorted(stat_options.keys())), multiple=True,
+              help='Name of the statistics value(s) that should be parsed from the report files')
+@click.option('-t', '--separate-tables', default=False, is_flag=True, help="Show all stats in separate tables")
+def stats(report_dir, separate_tables, glob_strings, output_dir, stat):
+    """Get stats from the generated report files
     \f
 
+    :param separate_tables: true if all stats should be displayed in their own table, false otherwise
     :param stat: name of the statistics value that should be parsed from the report files
     :param output_dir: output directory
-    :param output_format: output format (deprecated)
-    :param glob_string: glob string
+    :param glob_strings: glob string
     :param report_dir: report directory
     """
-    file_list = get_file_list(glob_string, report_dir)
 
-    file_handles = [open(fp, 'r') for fp in file_list]
+    file_list = get_file_list(glob_strings, report_dir)
+    stats_df = get_dataframe_from_file_list(file_list)
 
-    msg_stats = []
-    for fh in file_handles:
-        scenario = {'scenario': fh.readline().split(' ')[-1].strip()}
-        for line in fh.readlines():
-            elems = line.split(':')
-            if len(elems) > 1:
-                scenario[elems[0].strip()] = elems[1].strip()
-        msg_stats.append(scenario)
-        fh.close()
+    display_all = '*' in stat
 
-    stats_df = pd.DataFrame(msg_stats).set_index('scenario')
-
-    for col in stats_df.columns:
-        if col != 'scenario':
-            stats_df[col] = pd.to_numeric(stats_df[col], errors='coerce')
-
-    print(f'{file_list=}, {type(file_list)}')
-    print(f'{output_format=}, {type(output_format)}')
-    print(f'{output_dir}, {type(output_format)}')
-    print(stats_df)
+    cols = list(stats_df.columns) if display_all else list(stat)
+    if separate_tables:
+        for col in cols:
+            print(stats_df[[col]])
+    else:
+        print(stats_df[cols])
